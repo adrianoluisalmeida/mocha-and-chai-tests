c423c001de3201ece83145eb77aee15896fdf924
Adriano <ala@vortx.com.br>
2020-08-22 21:06:04 -0300

    remove old packages

--- api-testavel/README.md
+++
@@ -1,5 +0,0 @@
-## To perfom the tests
-
-1. Install mongodb and set configurations server.js  
-2. Run npm install
-3. Run npm server or npm test
--- api-testavel/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var app = require('./config/express');
-require('./config/database')('mongodb://localhost:27017/tests');
-
-http.createServer(app).listen(3000, function(){
-  console.log('Servidor iniciado...')
-});
